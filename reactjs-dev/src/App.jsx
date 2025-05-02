573f08774e06f79903f48f689a9a286cbe713f0b
Quaad <ajwood1998@hotmail.co.uk>
2025-05-02 16:45:36 +0200

    rendered components by passing them as props to the layout componnet. Added the header and footer to display the title of the website and a link in the footer to my gibhub page. Added cdn from font awesome to display a github icon from fontawesomeicons.

--- reactjs-dev/src/App.jsx
+++ reactjs-dev/src/App.jsx
@@ -1,11 +1,17 @@
-
+import Clocks from "./components/Clocks"
+import Layout from './components/layouts/Layout'
+import Hero from './components/Hero'
+import Calendar from './components/Calendar'
+import Summary from './components/Summary'
 
 function App() {
-
   return (
-    <div>
-        sdfsdfedfsfd
-    </div>
+    <Layout>
+        <Hero />
+        <Clocks />
+        <Calendar />
+        <Summary />
+    </Layout>
   )
 }
 
--- reactjs-dev/src/components/layouts/Layout.jsx
+++ reactjs-dev/src/components/layouts/Layout.jsx
@@ -1,5 +1,33 @@
-export default function Layout() {
+export default function Layout(props) {
+    const { children } = props
+
+    const header = (
+        <header>
+            <h1 className="text-medium text-gradient">
+                Unalive
+            </h1>
+        </header>
+    )
+
+    const footer = (
+        <footer>
+            <small>Created by: </small>
+            <a target="_blank" href="https://github.com/Quaad4"><img alt="pfp" src="https://avatars.githubusercontent.com/u/77070436?v=4" /> 
+                <p>
+                    @Quaad4
+                </p>
+                <i className="fa-brands fa-github" />
+            </a>
+        </footer>
+    )
+
     return (
-        <div></div>
+        <>
+            {header}
+                <main>
+                    {children}
+                </main>
+            {footer}            
+        </>
     )
 }
