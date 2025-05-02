eb12a3a643269553a017960b5a3fc2f174a2d984
Quaad <ajwood1998@hotmail.co.uk>
2025-05-02 20:48:05 +0200

    added content to the hero. Added initial setup of buttons and progress bar with static values for now until later when it will become dynamic based on user input

--- reactjs-dev/src/components/Hero.jsx
+++ reactjs-dev/src/components/Hero.jsx
@@ -1,7 +1,30 @@
 export default function Hero() {
+    const name = 'Alex'
+    const weeks = 2604
+    const percetange = '45%'
     return (
-        <div>
-            test
-        </div>
+        <section id="hero">
+            <h3 className="text-large">
+                {name}, you have {weeks} weeks left. Make them count 🫡
+            </h3>
+            <div className="btns-container">
+                <button>Not {name}?</button>
+                <button className="link-button">Copy link</button>
+                <button className="link-button">Reset</button>
+            </div>
+            <div className="progress-bar">
+                <div>
+                    <div>
+                        <i className="fa-solid fa-baby" />
+                        <h6 className="bar-label">Birth</h6>
+                    </div>
+                    <h6>{percetange}</h6>
+                </div>
+                <div>
+                    <h6 className="bar-label">Death</h6>
+                    <i className="fa-solid fa-skull" />
+                </div>
+            </div>
+        </section>
     )
 }
