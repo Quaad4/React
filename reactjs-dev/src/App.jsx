db3e1d794a75cd3e81c05081e8ac346fd68f3fb7
Quaad <ajwood1998@hotmail.co.uk>
2025-05-03 22:56:55 +0200

    updated layout to now contain toplevel data and pass data down to child components as props. Now displaying data from parent in the hero section.

--- reactjs-dev/src/App.jsx
+++ reactjs-dev/src/App.jsx
@@ -3,11 +3,18 @@ import Layout from './components/layouts/Layout'
 import Hero from './components/Hero'
 import Calendar from './components/Calendar'
 import Summary from './components/Summary'
+import { calculateTimeLeft, getLifePercentageLived } from "./utils"
 
 function App() {
+  const name = 'Alex'
+  const birthDate = '1995-06-15'
+  const lifeExpectancy = 80
+  const percentage = getLifePercentageLived(birthDate, lifeExpectancy)
+  const data = calculateTimeLeft(birthDate, lifeExpectancy)
+
   return (
     <Layout>
-        <Hero />
+        <Hero name={name} data={data} percentage={percentage}/>
         <Clocks />
         <Calendar />
         <Summary />
