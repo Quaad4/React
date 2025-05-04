baeb66c705bbb128f5384960ef9f5fa01d673339
Quaad <ajwood1998@hotmail.co.uk>
2025-05-04 20:30:55 +0200

    added content the summary, using some logic to get the final year through parsing the birthdate and splitting it to fetch the zeroth index

--- reactjs-dev/src/components/Summary.jsx
+++ reactjs-dev/src/components/Summary.jsx
@@ -1,5 +1,16 @@
-export default function Summary() {
+export default function Summary(props) {
+    const { lifeExpectancy, birthDate } = props
+    const finalYear = parseInt(birthDate.split('-')[0]) + lifeExpectancy
     return (
-        <div></div>
+        <section id="summary">
+            <div>
+                <p className="text-gradient">
+                    <i className="fa-solid fa-heart-crack" />
+                    You&apos;ll probably die in the year <strong>{finalYear}</strong> at the age of <strong>{lifeExpectancy}.</strong>
+                </p>
+            </div>
+            <h4>Yesterday is history, tomorrow is a mystery and today is a gift.<br />That&apos;s why they call it the present</h4>
+            <h4>Master Oogway</h4>
+        </section>
     )
 }
